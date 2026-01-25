6cb1f9553590ea01669a863f1ee881207a852f8a
chong-23 <chonghu23@gmail.com>
2026-01-24 22:35:26 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'chong_portfolio_2026', // Replace 'your-repo-name' with your actual repository name
+  base: "/chong_portfolio_2026/", // This matches your repo name exactly
   plugins: [
-    react(), 
-    mode === "development" && componentTagger()
+    react(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
-  
   server: {
     host: "::",
     port: 8080,
   },
-  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  
 }));
